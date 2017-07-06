3cd530f3008e7d863f8b8d6f58f9bde12f5fcfc5
Alan Mackenzie <bug-cc-mode@gnu.org>
2017-07-06 18:13:43 +0000

    CC Mode: create and use c-set-keymap-parent.
    * cc-defs.el (c-set-keymap-parent): New macro.
    
    * cc-mode.el (top-level): Remove cc-bytecomp-defun for set-keymap-parents.
    (c-make-inherited-keymap): Use c-set-keymap-parent in place of inline code.

--- cc-mode.el
+++ cc-mode.el
@@ -95,7 +95,6 @@
 
 ;; Silence the compiler.
 (cc-bytecomp-defvar adaptive-fill-first-line-regexp) ; Emacs
-(cc-bytecomp-defun set-keymap-parents)	; XEmacs
 (cc-bytecomp-defun run-mode-hooks)	; Emacs 21.1
 (cc-bytecomp-obsolete-fun make-local-hook) ; Marked obsolete in Emacs 21.1.
 (cc-bytecomp-defvar normal-erase-is-backspace)
@@ -214,18 +213,7 @@ control).  See \"cc-mode.el\" for more info."
 
 (defun c-make-inherited-keymap ()
   (let ((map (make-sparse-keymap)))
-    ;; Necessary to use `cc-bytecomp-fboundp' below since this
-    ;; function is called from top-level forms that are evaluated
-    ;; while cc-bytecomp is active when one does M-x eval-buffer.
-    (cond
-     ;; XEmacs
-     ((cc-bytecomp-fboundp 'set-keymap-parents)
-      (set-keymap-parents map c-mode-base-map))
-     ;; Emacs
-     ((cc-bytecomp-fboundp 'set-keymap-parent)
-      (set-keymap-parent map c-mode-base-map))
-     ;; incompatible
-     (t (error "CC Mode is incompatible with this version of Emacs")))
+    (c-set-keymap-parent map c-mode-base-map)
     map))
 
 (defun c-define-abbrev-table (name defs)
