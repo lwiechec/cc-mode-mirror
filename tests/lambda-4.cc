e00c4f9257db88d54d4225579fdf43bba802ff2c
Alan Mackenzie <bug-cc-mode@gnu.org>
2017-12-14 20:55:10 +0000

    Fix fontification of first declaration within a C++ lambda form.
    * cc-engine.el (c-looking-at-or-maybe-in-bracelist): Cease spuriously
    recognizing the braces of a lambda form as a brace list when there is an "="
    preceding the introductory brackets.
    
    * lambda-4.{cc,face}: New test files.

---
+++ tests/lambda-4.cc
@@ -0,0 +1,9 @@
+struct SomeType { };
+struct SomeOther { SomeType someMethod(void)const{return SomeType();} };
+int main()
+{
+    auto someLambda = [&]( const SomeOther& access )
+                      {
+                          const SomeType& someType = access.someMethod();
+                      };
+}
