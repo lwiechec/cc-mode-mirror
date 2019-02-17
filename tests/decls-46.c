bee929a085d788e429933355e6c9951e33d27697
Alan Mackenzie <bug-cc-mode@gnu.org>
2019-02-17 18:09:53 +0000

    Fontify C function identifiers in parentheses correctly (e.g. in lisp.h)
    Fix handling of CC Mode's syntactic WS cache.  Make noise-macro option
    variables buffer local.
    
    * cc-engine.el (c-put-is-sws, c-put-in-sws, c-remove-is-sws, c-remove-in-sws)
    (c-remove-is-and-in-sws): Add edebug specs.
    (c-invalidate-sws-region-before): Add a `beg' parameter.  Handle noise
    macros like other literals.
    (c-invalidate-sws-region-after-del): Move the adjustment of (cdr
    c-sws-lit-limits) due to buffer change to c-invalidate-sws-region-after.
    (c-invalidate-sws-region-after-ins): Move (goto-char end) to the correct
    place.
    (c-invalidate-sws-region-after): Adjust (cdr c-sws-lit-limits) due to buffer
    change.  Handle noise macros.
    (c-backward-sws): Set simple-ws-beg appropriately when the start point is in
    the middle of a noise macro.
    (c-forward-decl-or-cast-1): Recognize a function identifier being declared in
    parentheses.
    
    * cc-mode.el (c-before-change): Supply a `beg' argument to
    c-invalidate-sws-region-before.
    
    * cc-vars.el (c-noise-macro-with-parens-name-re, c-noise-macro-name-re)
    (c-noise-macro-names, c-noise-macro-with-parens-names): Make these buffer
    local variables.
    
    * decls-46.{c,face}: New test files.

---
+++ tests/decls-46.c
@@ -0,0 +1,16 @@
+bool
+(SYMBOLP) (Lisp_Object x)
+{
+  return lisp_h_SYMBOLP (x);
+}
+
+INLINE bool
+(SYMBOLP) (Lisp_Object x)
+{
+  return lisp_h_SYMBOLP (x);
+}
+
+/* Local Variables: */
+/* c-noise-macro-names: ("INLINE") */
+/* eval: (c-make-noise-macro-regexps) */
+/* End: */
