29413b4f7e9952051ff63eb31c8358d25b865f4d
Alan Mackenzie <bug-cc-mode@gnu.org>
2019-03-30 12:50:28 +0000

    Allow a CC Mode derived mode to have strings delimited by single quotes.
    Also fix the bug where the delimiters of '\033', etc. got the error face.
    
    * cc-langs.el (c-single-quotes-quote-strings): Enhance the doc string.
    (c-string-delims): Change doc string to doc comment.
    
    * cc-mode.el (c-before-change-check-unbalanced-strings): In searches and
    comparisons, take account of the string delimiters possibly being '.  Fix
    argument in call of c-before-change-check-unbalanced-strings.
    (c-parse-quotes-before-change, c-parse-quotes-after-change): Bind
    case-fold-search to nil.  Analyze escape constructs inside character constants
    more accurately, in particular accepting as valid more than one character
    after /[0-7], /x, /u, and /U.  Amend calculations to account for this extra
    length.
    
    * constants-4.{c,face}: New test files.

---
+++ tests/constants-4.c
@@ -0,0 +1,12 @@
+char foo = 27;
+char foo = '\0';
+char foo = '\033';
+char foo = '\x1b';
+char foo = '\x1B00';
+char foo = '\123';
+char foo = '\u201a';
+char foo = '\U201a';
+
+char foo = '\0123';
+char foo = '\x1g';
+char foo = '\X1b';
