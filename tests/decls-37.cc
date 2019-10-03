b1efe464c377c2af064698feec7f2f3c0641929d
Alan Mackenzie <bug-cc-mode@gnu.org>
2019-10-03 14:14:35 +0000

    C++ Mode: Correct the fontification of const auto foo
    * cc-engine.el (c-forward-decl-or-cast-1): Correct a coding error involving
    `and' and `or'.
    
    * decls-37.{cc,face}: Insert two extra lines to test the current scenario.

--- tests/decls-37.cc
+++ tests/decls-37.cc
@@ -1,6 +1,8 @@
 // C++11 constructs
 decltype (BAR) bar;
 auto foo = bar;
+const auto foo = bar;
 auto foo () -> BAZ;
+auto foo () -> const BAZ;
 auto foo (BAR bar) -> BAZ;
 auto foo (BAR bar, BAZ baz) -> decltype (bar + baz);
