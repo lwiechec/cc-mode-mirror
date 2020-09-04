8ef745e0d99acd2c7c8ffac59fb050d1342d79f8
Alan Mackenzie <bug-cc-mode@gnu.org>
2020-09-04 18:50:14 +0000

    C++ Mode: handle comma separated brace initializers.
    This includes both indentation and fontification.
    
    * cc-engine.el (c-do-declarators): Handle brace initializers without =
    correctly.
    (c-looking-at-or-maybe-in-bracelist): Use c-do-declarators with a simple
    inline function to check that after-type-id-pos points to the start of a
    declarator.
    
    * cc-langs.el (c-recognize-bare-brace-inits): New lang const/variable.
    
    * bracelist-35.{cc,res,face}: New test files.

---
+++ tests/bracelist-35.cc
@@ -0,0 +1,15 @@
+int main ()
+{
+    vector<int>
+        bla{1, 2, 3,
+        4, 5, 6},
+        bla2{1, 2, 3,
+        4, 5, 6};
+    return 0;
+}
+
+int main ()
+{
+    int a{1}, b{1};
+    return 0;
+}
